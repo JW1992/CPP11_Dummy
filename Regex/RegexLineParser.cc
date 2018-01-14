1535291c3bdb97a2fb6f2cd7f2bf52ce50af055c
JW1992 <jiaweix@umich.edu>
2018-01-14 17:00:23 -0500

    Dummy settings.ini input parser using simple regex

--- Regex/RegexLineParser.cc
+++ Regex/RegexLineParser.cc
@@ -25,23 +25,57 @@ int main ()
   	
 	std::ifstream infile("Settings.ini");//No need to release the memory manually this way
 	std::string line;
-	int nTemp1 = 0;
-	int nTemp2 = 0;
+	
+    std::regex rComment ("^#");
+    std::regex rBlank ("^\s*$");
+    std::regex rTime ("^SIM_TIME");
+    std::regex rPointSouce ("^SOURCE_POINT");
+    std::regex rCuboidObject ("^OBJ_CUBOID");
+    std::smatch m;
+    std::string strKeyWord;
+    
+    bool bIfInputValid = true;
+	
 	while (std::getline(infile, line))
 	{
-		/*
-    	std::istringstream iss(line);
-    	int a, b;
-    	if (!(iss >> a >> b)) { break; } // error
-    	*/
-		//std::cout<<line<<std::endl;
-    	std::regex rComment ("^#");
-    	std::regex rBlank ("^\s*$");
-    	std::smatch m;
-    	if(std::regex_search (line,m,rComment)) nTemp1++;
-    	if(std::regex_search (line,m,rBlank)) nTemp2++;
+    	if(std::regex_search (line,m,rComment)) continue;
+    	if(std::regex_search (line,m,rBlank)) continue;
+    	//Input time length
+    	if(std::regex_search (line,m,rTime)){
+    		std::istringstream iss(line);
+    		if(!(iss >> strKeyWord)) bIfInputValid = false;
+    		double fTime = 0.0;
+    		if(!(iss >> fTime)) bIfInputValid = false;
+    		std::cout<<"Simulation time length: "<<fTime<<" seconds"<<std::endl;
+    	}
+    	//Input source type
+    	if(std::regex_search (line,m,rPointSouce)){
+    		std::istringstream iss(line);
+    		if(!(iss >> strKeyWord)) bIfInputValid = false;
+    		double x,y,z,fA;
+    		if(!(iss >> x >> y >> z >>fA)) bIfInputValid = false;
+    		std::cout<<"Add point source, x="<<x<<", y="<<y<<", z="<<z;
+    		std::cout<<", Activity: "<<fA<<" uCi."<<std::endl;
+    	}
+    	//Input object type
+    	if(std::regex_search (line,m,rCuboidObject)){
+    		std::istringstream iss(line);
+    		if(!(iss >> strKeyWord)) bIfInputValid = false;
+    		double x1,y1,z1,x2,y2,z2;
+    		std::string strMaterialName;
+    		int nIsDetector;
+    		if(!(iss >> x1 >> y1 >> z1 >> x2 >> y2 >> z2)) bIfInputValid = false;
+    		if(!(iss >> strMaterialName)) bIfInputValid = false;
+    		if(!(iss >> nIsDetector)) bIfInputValid = false;
+    		std::cout<<"Add new object: cuboid of "<<strMaterialName;
+    		if(nIsDetector>0) std::cout<<" as active detctor volume.";
+    		std::cout<<std::endl;
+    		std::cout<<"Position: "<<x1<<" "<<y1<<" "<<z1<<" "<<x2<<" "<<y2<<" "<<z2<<" "<<std::endl;
+    	}
+    	if(!bIfInputValid){
+    		std::cout<<"Input file exception"<<std::endl;
+    		return 0;
+    	}
 	}
-	std::cout<<nTemp1<<std::endl;
-	std::cout<<nTemp2<<std::endl;
 	return 0;
 }
