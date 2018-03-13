6318124423955967ec36dacc75f62b9a88fe479e
JW1992 <jiaweix@umich.edu>
2018-03-12 22:29:10 -0400

    Template noob

--- Template/Template.cc
+++ Template/Template.cc
@@ -6,8 +6,18 @@ T GetMax(T a, T b){
 	return a>b?a:b;
 }
 
+template <class T>
+struct MyPair{
+	T p[2];
+	MyPair(T a, T b){
+		p[0]=a; p[1]=b;
+	}
+};
+
 int main(){
 	int a=1, b=2;
 	std::cout<< GetMax(a,b) <<std::endl;
 	std::cout<< GetMax<int>(a,b) <<std::endl;
+	MyPair<double> foo(0.0, 0.1);
+	std::cout << foo.p[0] << " " << foo.p[1] <<std::endl;
 }
