da250a39bf9cf86aa5ff96f6fb607813d746609b
JW1992 <jiaweix@umich.edu>
2018-06-25 22:30:55 -0400

    foo

--- Template/Template.cc
+++ Template/Template.cc
@@ -1,16 +1,31 @@
 #include <iostream>
 
 
-template <class T>
+template <typename T>
 T GetMax(T a, T b){
 	return a>b?a:b;
 }
 
-template <class T>
-struct MyPair{
+template<class T, class U>
+class DoubleTempParClass{
+private:
+	T t;
+	U u;
+public:
+	DoubleTempParClass(T inT, U inU):t(inT),u(inU){}
+	T getT(){return t;}
+	T getU(){return u;}
+};
+
+template <typename T>
+class MyPair{
+public:
 	T p[2];
 	MyPair(T a, T b){
 		p[0]=a; p[1]=b;
+	}
+	static const double Pi(){
+		return 3.14;
 	}
 };
 
@@ -20,4 +35,8 @@ int main(){
 	std::cout<< GetMax<int>(a,b) <<std::endl;
 	MyPair<double> foo(0.0, 0.1);
 	std::cout << foo.p[0] << " " << foo.p[1] <<std::endl;
+	std::cout << MyPair<int>::Pi() <<std::endl;
+	DoubleTempParClass<int, double> DTPC{1, 2.0};
+	std::cout << DTPC.getT() <<std::endl;
+	std::cout << DTPC.getU() <<std::endl;
 }
