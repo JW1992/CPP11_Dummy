9c6ef0427c96a61cf50a5dcc179e11b6f1972a10
JW1992 <jiaweix@umich.edu>
2018-06-26 23:33:36 -0400

    foo

--- QT_CPP/QT_CPP/main.cpp
+++ QT_CPP/QT_CPP/main.cpp
@@ -1,9 +1,25 @@
 #include <QCoreApplication>
+#include <iostream>
 #include "mysharedptr.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    MySharedPtr<int> foo1(new int{1});
+    std::cout<<foo1.getRefCnt()<<std::endl;
+
+    auto foo2 = foo1;
+    std::cout<<foo2.getRefCnt()<<std::endl;
+
+    foo2 = foo1;
+    std::cout<<foo2.getRefCnt()<<std::endl;
+
+    MySharedPtr<int> foo3(new int{2});
+    foo3 = foo1;
+
+    std::cout<<foo2.getRefCnt()<<std::endl;
+    std::cout<< *foo2 <<std::endl;
 
     return a.exec();
 }
--- QT_CPP/QT_CPP/mysharedptr.h
+++ QT_CPP/QT_CPP/mysharedptr.h
@@ -1,23 +1,95 @@
 #ifndef MYSHAREDPTR_H
 #define MYSHAREDPTR_H
+#include <iostream>
+#include <sstream> //for std::stringstream
+#include <string>  //for std::string
 
 //Class template for my smart pointer
 template <typename T>
 class MySharedPtr{
 public:
-    MySharedPtr(const T& obj_){
-        ref_cnt = 1;
-        pObj = new T(obj);
+    MySharedPtr(T* obj_){
+        const void * address = static_cast<const void*>(this);
+        std::stringstream ss;
+        ss << address;
+        std::cout<<ss.str()<<" ";
+        std::cout<<"Default constructor."<<std::endl;
+
+        pRefCnt=new unsigned int{1};
+        pObj = obj_;
+    }
+    //Copy constructor: previously uninitialized.
+    MySharedPtr(MySharedPtr<T> &ptr_){
+        const void * address = static_cast<const void*>(this);
+        std::stringstream ss;
+        ss << address;
+        std::cout<<ss.str()<<" ";
+        std::cout<<"Copy constructor."<<std::endl;
+
+        pRefCnt = ptr_.pRefCnt;
+        ++(*pRefCnt);
+        pObj = ptr_.pObj;
+    }
+    //Assignment operator: replacement
+    //The assignment operator reuturns an lvalue reference to avoid overhead in chain assignments,
+    //such that objA = objB = objC works.
+    MySharedPtr<T> & operator = (MySharedPtr<T> &ptr_){
+        const void * address = static_cast<const void*>(this);
+        std::stringstream ss;
+        ss << address;
+        std::cout<<ss.str()<<" ";
+        std::cout<<"Assignment constructor."<<std::endl;
+
+        if(this == &ptr_) return *this;
+
+        if(*pRefCnt>0) remove();
+
+        pRefCnt = ptr_.pRefCnt;
+        ++(*pRefCnt);
+        pObj = ptr_.pObj;
+
+        return *this;
     }
     ~MySharedPtr(){
-        --ref_cnt;
-        if(!ref_cnt){
-            delete pObj;
-        }
+        remove();
+    }
+    std::string getRefCnt(){
+        const void * address = static_cast<const void*>(this);
+        std::stringstream ss;
+        ss << address;
+        ss << " reference counts: ";
+        ss << *pRefCnt;
+        return ss.str();
+    }
+    //Dereference
+    //Prefix operator because there are not input parameters.
+    T& operator * (){
+        return *pObj;
+    }
+    //Mmeber access operator
+    //Prefix operator
+    //"drill-down behavior"???
+    T* operator -> (){
+        return pObj;
     }
 private:
-    T *pObj;
-    int ref_cnt;
+    T *pObj = nullptr;
+    unsigned int *pRefCnt=nullptr;
+
+    void remove(){
+        --(*pRefCnt);
+        if(*pRefCnt==0){
+            const void * address = static_cast<const void*>(this);
+            std::stringstream ss;
+            ss << address;
+            std::cout<<ss.str()<<" ";
+            std::cout<<"Reference count is zero. Delete."<<std::endl;
+            delete pRefCnt;
+            delete pObj;
+            pRefCnt = nullptr;
+            pObj = nullptr;
+        }
+    }
 };
 
 #endif // MYSHAREDPTR_H
